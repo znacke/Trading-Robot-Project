6523122eb6c4a551dcb3a81ffb1ed29e54b47628
Adrian <Adrian@10.192.100.235>
2018-12-20 15:00:50 +0100

    kunde

--- src/Kunde.java
+++ src/Kunde.java
@@ -1,25 +1,7 @@
 import java.time.LocalDate;
 
 public class Kunde {
-<<<<<<< HEAD
 
-/*	private String Vorname;
-=======
-	public static void main (String[] args) {	
-/*	
-	private String Vorname;
->>>>>>> branch 'Prototyp' of https://github.com/znacke/Trading-Robot-Project
-	private String Nachname;
-	private String Strasse;
-	private int Hausnummer;
-	private String PrivatKontoNR;
-	private String EMail;
-	private int Telefon;
-	private boolean Bonitaet;
-	private boolean Einlage;
-	
-	*/
-			
 	private String vorname = "Christian";
 	private String nachname = "Gross";
 	private String strasse = "Strasse";
@@ -30,47 +12,5 @@ public class Kunde {
 	private boolean bonitaet;
 	private boolean einlage;
 
-	
-	
-	
-	/*
->>>>>>> branch 'Prototyp' of https://github.com/znacke/Trading-Robot-Project
-	int day = 10;
-	int month = 11;
-<<<<<<< HEAD
-	int year = 1986; 
-	
-
-	if(year<=2018-18)
-	{
-=======
-	int year = 1986;
-
-		
-	if (year <= 2018-18) {
->>>>>>> branch 'Prototyp' of https://github.com/znacke/Trading-Robot-Project
-		System.out.println("is of age");
-<<<<<<< HEAD
-	}else
-	{
-=======
-		if (month <= 11) {
-			System.out.println("is of age");
-		}
-	}else {
->>>>>>> branch 'Prototyp' of https://github.com/znacke/Trading-Robot-Project
-		System.out.println("IS NOT");
-	}
-<<<<<<< HEAD
-=======
-	*/	
-		
->>>>>>> branch 'Prototyp' of https://github.com/znacke/Trading-Robot-Project
-
-	/*
-	 * Vorname:String Nachname:String Strasse : String Hausnummer : int Privat Konto
-	 * : String E-Mail : String Alter : int Telefon : int Bonität : boolean Einlagen
-	 * : boolean Aktienliste [ ] <------------ Aktie bestimmen () <----------
-	 */
-
-}}
+}
+}
