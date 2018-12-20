d5f3acfb8b43e280039d5f428e78c99c76149aeb
znack <znack@10.192.100.173>
2018-12-20 15:07:17 +0100

    age calc

--- src/Alter.java
+++ src/Alter.java
@@ -1,21 +1,31 @@
+import java.time.LocalDate;
+import java.time.Period;
 
-public class Alter extends AgeCalculator{
-	
+public class Alter {
+
 	int jahr;
 	int monat;
 	int tag;
-	
-	
-	
+	LocalDate geburtsdatum;
+
+	public static LocalDate getLocalDate() {
+		return LocalDate.now();
+
+	}
+
 	Alter(int jahr, int monat, int tag) {
 		this.jahr = jahr;
 		this.monat = monat;
 		this.tag = tag;
-		
-		
-	try {
-		
+		geburtsdatum = LocalDate.of(jahr, monat, tag);
+
 	}
+
+	public boolean checkAge18() {
+		if (Period.between(geburtsdatum, getLocalDate()).getYears() > 18) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
 }
