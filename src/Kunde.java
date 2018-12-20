19c915ff5cfb9cd3d837ff125f992711df9e108a
HP <HP@10.192.102.166>
2018-12-20 14:13:28 +0100

    Bojan, Zuweisungen für Klasse Kunde

--- src/Kunde.java
+++ src/Kunde.java
@@ -1,10 +1,8 @@
 import java.time.LocalDate;
 
 public class Kunde {
-	
-	public static void main (String[] args) {	
-/*	
-	private String Vorname;
+
+/*	private String Vorname;
 	private String Nachname;
 	private String Strasse;
 	private int Hausnummer;
@@ -13,47 +11,38 @@ public class Kunde {
 	private int Telefon;
 	private boolean Bonitaet;
 	private boolean Einlage;
+	
 	*/
+
+	private String Vorname = "Christian";
+	private String Nachname = "Gross";
+	private String Strasse = "Lorenweg";
+	private int Hausnummer = 27;
+	private String PrivatKontoNr = "123-456";
+	private String Email = "abc";
+	private int Telefon = +791234567;
+	private boolean Bonitaet = true;
+	private boolean Einlage = true;
+	
+	
 
 	int day = 10;
 	int month = 11;
-	int year = 1986;
-		
+	int year = 1986; 
 	
-	if (year <= 2018-18) {
+
+	if(year<=2018-18)
+	{
 		System.out.println("is of age");
-	}else {
+	}else
+	{
 		System.out.println("IS NOT");
 	}
-		
-		
 
+	/*
+	 * Vorname:String Nachname:String Strasse : String Hausnummer : int Privat Konto
+	 * : String E-Mail : String Alter : int Telefon : int Bonität : boolean Einlagen
+	 * : boolean Aktienliste [ ] <------------ Aktie bestimmen () <----------
+	 */
 
-	
-
-	
-	
-	
-	
-	
-	
-
-
-			
-		
-/* Vorname:String
-Nachname:String
-Strasse : String
-Hausnummer : int
-Privat Konto : String
-E-Mail : String
-Alter : int
-Telefon : int
-Bonität : boolean
-Einlagen : boolean
-Aktienliste [ ] <------------
-Aktie bestimmen ()  <----------
- */
-	
-	}
-}
+}}
