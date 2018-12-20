50877b1370d6960d19aa397c6c89298e11b658da
Adrian <Adrian@10.192.100.235>
2018-12-20 13:53:13 +0100

    Aktie

--- src/Aktie.java
+++ src/Aktie.java
@@ -2,13 +2,19 @@
 public class Aktie {
 
 	public static void main(String[] Args) {
-		
-/*Name : String
-Wert : double
-Max. : double
-Min. : double
-Volumen : int
-check Schwellenwert ( )
-*/
+
+		// max wird fuer Verkauf genutzt
+		// min wird fuer Kauf genutzt
+		// volumenk ist das Volumen für den Kauf
+		// volumenv ist das Volumen für den Verkauf
+		// ES FEHLT DIE METHODE CHECKSCHWELLENWERT
+
+		String name = "name";
+		double wert = 100.58;
+		double max = 150.33;
+		double min = 80.05;
+		int volumenk = 2;
+		int volumenv = 2;
+
 	}
 }
