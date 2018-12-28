c414f2d955f65f6aba2885a1a41be90acbb81053
Adrian <Adrian@LAPTOP-3N3A1U8L>
2018-12-28 10:59:37 +0100

    ..

--- src/WeitereKlasse1.java
+++ src/WeitereKlasse1.java
@@ -1,4 +1,6 @@
-	public class StockTransaction
+import java.util.Scanner;
+
+public class WeitereKlasse1
 	{
 	    public static void main(String[]args) {
 	        String name;
@@ -23,4 +25,4 @@
 	    }
 	}
 
-}
+
