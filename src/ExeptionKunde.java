9f5eba5e94f58596679d6199c65b275e03398306
znack <znack@DESKTOP-RJDBOOR>
2018-12-28 10:29:55 +0100

    kunde exception

--- src/ExeptionKunde.java
+++ src/ExeptionKunde.java
@@ -1,12 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class ExeptionKunde {
-	
-	public static void main(String[] args) {
-		
-/*		Alter < 18 
-		Bonität false
-		Einlage < 200'000.- Chf
-		*/
-	}
+
+  public static void main(String[] a) {
+    JFrame frame = new JFrame();
+    Object result = JOptionPane.showInputDialog(frame, "Bonität: True/False: ");
+
+    System.out.println(result);
+    	
+    if (result = boolean) {
+    	this.infoBox("True, Bonität des Kunden ist gut");
+    	
+    }
+    	
+  }
 
 }
