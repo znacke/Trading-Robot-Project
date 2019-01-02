636ae874ecde516177c9c97c7f898204547e4d8b
HP <HP@192.168.0.227>
2019-01-02 17:20:32 +0100

    Klasse Kunde erweiter //BOJAN

--- src/Kunde.java
+++ src/Kunde.java
@@ -1,18 +1,140 @@
 import java.time.LocalDate;
 
 public class Kunde {
+	
+	//Eigenschaften
 
-	private String vorname = "Christian";
-	private String nachname = "Gross";
-	private String strasse = "Strasse";
-	private int hausnummer = 214;
-	private String privatKontoNR = "112-458";
-	private String eMail = "christian.gross@gmx.ch";
-	private int telefon = +794589684;
+	private String vorname;
+	private String nachname;
+	private String strasse;
+	private int hausnummer;
+	private String privatKontoNr;
+	private String email;
+	private String telefon;
 	private boolean bonitaet;
 	private boolean einlage;
-	private Alter a;
+	private int alter; //abgeändert wegen getter und setter
+	
+	
+	//Konstruktoren
+	
+	public Kunde() {
+		
+	}
+	
+	public Kunde(String vorname, String nachname, String strasse, int hausnummer, String privatKontoNr, String email, String telefon, boolean bonitaet, boolean einlage, int alter) {
+		this.setVorname(vorname);
+		this.setNachname(nachname);
+		this.setStrasse(strasse);
+		this.setHausnummer(hausnummer);
+		this.setPrivatKontoNr(privatKontoNr);
+		this.setemail(email);
+		this.setTelefon(telefon);
+		this.setBonitaet(bonitaet);
+		this.setEinlage(einlage);
+		this.setAlter(alter);
+	}
+
+	public String getVorname() {
+		System.out.println("Der Vorname des Kunden ist: " + vorname);
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getNachname() {
+		System.out.println("Der Nachname des Kunden ist: " + nachname);
+		return nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public String getStrasse() {
+		System.out.println("Die Strasse des Kunden ist: " + strasse);
+		return strasse;
+	}
+
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+
+	public int getHausnummer() {
+		System.out.println("Die Hausnummer des Kunden ist: " + hausnummer);
+		return hausnummer;
+	}
+
+	public void setHausnummer(int hausnummer) {
+		this.hausnummer = hausnummer;
+	}
+
+	public String getPrivatKontoNr() {
+		System.out.println("Die Kontonummer des Kunden ist: " + privatKontoNr);
+		return privatKontoNr;
+	}
+
+	public void setPrivatKontoNr(String privatKontoNr) {
+		this.privatKontoNr = privatKontoNr;
+	}
+
+	public String getemail() {
+		System.out.println("Die E-mail des Kunden ist: " + email);
+		return email;
+	}
+
+	public void setemail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefon() {
+		System.out.println("Die Telefonnummer des Kunden ist: " + telefon);
+		return telefon;
+	}
+
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+
+	public boolean isBonitaet() {
+		System.out.println("Die Bonität entspricht unseren Anforderungen");
+		return bonitaet;
+	}
+
+	public void setBonitaet(boolean bonitaet) {
+		this.bonitaet = bonitaet;
+	}
+
+	public boolean isEinlage() {
+		System.out.println("Die Einlage entspricht unseren Anforderungen");
+		return einlage;
+	}
+
+	public void setEinlage(boolean einlage) {
+		this.einlage = einlage;
+	}
+
+	public int getAlter() {
+		System.out.println("Das Alter des Kunden beträgt: " + alter);
+		return alter;
+	}
+
+	public void setAlter(int alter) {
+		this.alter = alter;
+	}
+	
 	
 
-}
+	
+
+	}
+	
+	
+	
+	
+	
+	
+
 
--- src/Mitarbeiter.java
+++ src/Mitarbeiter.java
@@ -1,14 +1,31 @@
 
 public class Mitarbeiter {
-	
+
 	public static void main(String[] args) {
 		
-/* KundenprofieErstellen()
- * auszahlen()
- * FondKontoEroefnen()
- * FondKontoAufloesen()
- * GebuehrenErheben()
- */
+
+		Kunde peter = new Kunde("Peter", "Muster", "Lorenweg", 27, "123-456", "peter.muster@hotmail.com", "078 704 51 89", true, true, 20);
+		peter.getVorname();
+		peter.getNachname();
+		peter.getStrasse();
+		peter.getHausnummer();
+		peter.getPrivatKontoNr();
+		peter.getemail();
+		peter.getTelefon();
+		peter.isBonitaet();
+		peter.isEinlage();
+		peter.getAlter();
+		
+	
+		
+	
+		
+			
+		}
 	}
 
-}
+		/*
+	 * KundenprofieErstellen() auszahlen() FondKontoEroefnen() FondKontoAufloesen()
+	 * GebuehrenErheben()
+	 */
+
