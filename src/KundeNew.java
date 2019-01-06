aacf6c6a84fbbe4565e54d0e9eb3a976ba9b8cd9
znacke <znacke@192.168.1.106>
2019-01-06 16:13:03 +0100

    Klasse KundeNew vorbereitet. Fehler beim dataimport. nimmt die daten aus dem text file nicht auf.

--- src/KundeNew.java
+++ src/KundeNew.java
@@ -1,15 +1,14 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import com.sun.xml.internal.fastinfoset.sax.Properties;
+import java.util.Properties;
 
 public class KundeNew {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 
 		if (args.length < 7)
-			throw new RuntimeException("Insufficient Arguments");
+			throw new RuntimeException("Insufficient input");
 		String mode = args[0];
 		if (mode.compareTo("Kundeerstellung") == 0) {
 			String kunde = args[1];
@@ -18,18 +17,24 @@ public class KundeNew {
 			String vorname = props.getProperty("Vorname");
 			String nachname = props.getProperty("Nachname");
 			String strasse = props.getProperty("Strasse");
-			int hausnummer = props.getProperty("Hausnummer");
+			String hausnummer = props.getProperty("Hausnummer");
 			String telefon = props.getProperty("Telefon");
 			String email = props.getProperty("EMail");
 			String privatkontonr = props.getProperty("Privatkontonummer");
 
-			double kontostand = Double.parseDouble(props.getProperty("Kontostand"));
+
 			System.out.println("Kunde: " + vorname + " " + nachname);
 			System.out.println("Wonwort: " + strasse + ", " + hausnummer);
 			System.out.println("Kontaktinformationen: " + "Telefon: " + telefon + ", " + "E-Mail: " + email);
 			System.out.println("Privatkonto Nr: " + privatkontonr);
+			
+//			double kontostand = Double.parseDouble(props.getProperty("Kontostand"));
+//			String ueberprueft = props.getProperty("ueberprueft");
+			
+//			System.out.println("Kunde wurde vom Mitarbeiter überprüft: " + ueberprueft);
+			
 		} 
-		else if (mode.comareTo("handeln") == 0) {
+		else if (mode.compareTo("handeln") == 0) {
 
 		}
 
