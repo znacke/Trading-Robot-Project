9749d2312b9c84a56d7b4179ff9578a60dd9364c
Adrian <Adrian@10.192.100.92>
2019-01-10 17:13:46 +0100

    Test

--- src/TestRobot.java
+++ src/TestRobot.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	
 	Robot r = new Robot ();
 	Aktie a = new Aktie ();
-	a.setWert(40);
+	a.setWert(60);
 	a.setMinSchwellenwert(50);
 	a.setMaxSchwellenwert(150);
 	a.setVolumen(2);
