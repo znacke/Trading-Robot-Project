35d04bb0a19f5b12881def3ca22a924b4ade3bb4
znack <znack@10.192.101.70>
2019-01-17 11:45:23 +0100

    exception kunde

--- src/AgeCalculator.java
+++ src/AgeCalculator.java
@@ -1,9 +1,17 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AgeCalculator {
 
 //Alterscheck des Kunden: Kunde => 18 Jahre
 
 	public static void main(String[] args) {
+		
+		 JPanel panel = new JPanel();
+	        panel.add(new JLabel("Bonität das Kunden: "));
+	        JTextField textField = new JTextField(10);
+	        panel.add(textField);
 		
 		Alter alter = new Alter(1992,12,28);
 		
