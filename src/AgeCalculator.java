61b4e7c026cc007d245816ac87fefa38390fcadb
znack <znack@10.192.101.70>
2019-01-17 10:53:09 +0100

    new push

--- src/AgeCalculator.java
+++ src/AgeCalculator.java
@@ -1,19 +1,12 @@
 
 public class AgeCalculator {
 
-	// test klasse zum altercheck des kunden. wird am ende ersetzt und/oder in der
-	// kundenklasse eingesetzt
+//Alterscheck des Kunden: Kunde => 18 Jahre
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		
-		Alter alter = new Alter(2006,12,28);
-=======
-
-		Alter alter = new Alter(2000, 12, 23);
-		;
-
->>>>>>> branch 'Prototyp' of https://github.com/znacke/Trading-Robot-Project
+		Alter alter = new Alter(1992,12,28);
+		
 		System.out.println(alter.checkAge18());
 
 	}
