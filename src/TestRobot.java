2d5bc5e475a0b738de5c7ce93cc69794098b3ab7
Adrian <Adrian@10.192.102.146>
2019-01-18 20:39:34 +0100

    Test

--- src/TestRobot.java
+++ src/TestRobot.java
@@ -6,10 +6,10 @@ public static void main(String[] args) {
 	Fondskonto f = new Fondskonto ();
 	Robot r = new Robot (f);
 	Aktie a = new Aktie ();
-	a.setWert(200);
+	a.setWert(40);
 	a.setMinSchwellenwert(50);
 	a.setMaxSchwellenwert(150);
-	a.setVolumen(2);
+	a.setVolumen(50);
 	
 	r.checkSchwellenwert(a);
 }
