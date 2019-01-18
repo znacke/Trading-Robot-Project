0bcd13444d20828c01c15203191d9c2d33fb0642
HP <HP@10.192.101.76>
2019-01-18 19:04:50 +0100

    Klasse Mitarbeiter mit Methode kundeerstellen ergänzt

--- src/Mitarbeiter.java
+++ src/Mitarbeiter.java
@@ -2,26 +2,60 @@
 public class Mitarbeiter {
 
 	public static void main(String[] args) {
-		
 
-		Kunde peter = new Kunde("Peter", "Muster", "Lorenweg", 27, "123-456", "peter.muster@hotmail.com", "078 704 51 89", true, true, 20);
-		Kunde hans = new Kunde("Hans", "Muster", "Musterstrasse1", 10, "132-654", "hans.muster@hotmail.com", "0781234567", true, true, 26);
-		
+//		Kunde peter = new Kunde("Peter", "Muster", "Lorenweg", 27, "123-456", "peter.muster@hotmail.com",
+//				"078 704 51 89", true, true, 20);
+//		Kunde hans = new Kunde("Hans", "Muster", "Musterstrasse1", 10, "132-654", "hans.muster@hotmail.com",
+//				"0781234567", true, true, 26);¨
+
+		Mitarbeiter m = new Mitarbeiter();
+		m.erstelleKunde("Bojan", "Eric", "Musterstrasse", 123, "abc-123", "bojan.eric@gmx.ch", "0765858585", true, false, 25);
+
 	}
-	
-	
+
+	public Kunde erstelleKunde(String vorname, String nachname, String strasse, int hausnummer, String privatKontoNr,
+			String email, String telefon, boolean bonitaet, boolean einlage, int alter) {
+		Kunde k = new Kunde("Hans", "Muster", "Musterstrasse", 1, "123-456", "muster@muster.ch", "781234567", true,
+				true, 22);
+
+		if (bonitaet == false || einlage == false) {
+
+		System.out.println("DER KUNDE KONNTE NICHT ERSTELLT WERDEN!");
+		} else 
+		System.out.println("Der Kunde " + vorname + " " + nachname + " wurde erstellt");
+		System.out.println("Vorname: " + vorname);
+		System.out.println("Nachname: " + nachname);
+		System.out.println("Alter: " + alter);
+		System.out.println("Strasse: " + strasse + hausnummer);
+		System.out.println("Bank Kontonummer: " + privatKontoNr);
+		System.out.println("Email Adresse: " + email);
+		System.out.println("Telefonnummer: " + telefon);
+		System.out.println("Bonität erfüllt? " + bonitaet);
+		System.out.println("Einlage erfüllt? " + einlage);
+
+		return k;
+
+	}
+
+	public static void auszahlenKonto() {
+
+	}
+
+	public static void erstelleFondsKonto() {
+
+	}
+
+	public static void auflösenFondsKonto() {
+
+	}
+
+	public static void erhebenGebuehren() {
+
+	}
+
 }
-		
-	
-		
-	
-		
-			
-		
-		
 
-		/*
-	 * KundenprofieErstellen() auszahlen() FondKontoEroefnen() FondKontoAufloesen()
-	 * GebuehrenErheben()
-	 */
-
+/*
+ * KundenprofieErstellen() auszahlen() FondKontoEroefnen() FondKontoAufloesen()
+ * GebuehrenErheben()
+ */
