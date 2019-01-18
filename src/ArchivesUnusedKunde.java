7ae246690347be822c6d6ef13e8d600ce4f7aa59
znack <znack@10.192.102.145>
2019-01-18 17:46:19 +0100

    nameing conventions

--- src/ArchivesUnusedKunde.java
+++ src/ArchivesUnusedKunde.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-public class KundeNewUNUSEDCLASS {
+public class ArchivesUnusedKunde {
 
 /*	public static void main(String[] args) throws FileNotFoundException, IOException {
 
