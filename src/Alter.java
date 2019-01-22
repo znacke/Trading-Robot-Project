3fe6b57ca91cc6d4893be35668b349d412cef939
znack <znack@10.192.102.70>
2019-01-22 18:02:17 +0100

    updated mit komentar

--- src/Alter.java
+++ src/Alter.java
@@ -3,7 +3,7 @@ import java.time.Period;
 
 public class Alter {
 
-	//klasse zum pruefen des alters des kunden
+//klasse zum pruefen des alters des kunden
 	int jahr;
 	int monat;
 	int tag;
@@ -11,16 +11,16 @@ public class Alter {
 
 	public static LocalDate getLocalDate() {
 		return LocalDate.now();
-
 	}
+
 // iniziert jahr monat und tag in eins
 	Alter(int jahr, int monat, int tag) {
 		this.jahr = jahr;
 		this.monat = monat;
 		this.tag = tag;
 		geburtsdatum = LocalDate.of(jahr, monat, tag);
-
 	}
+
 // alterscheck ob kunde 18+ oder nicht
 	public boolean checkAge18() {
 		if (Period.between(geburtsdatum, getLocalDate()).getYears() >= 18) {
