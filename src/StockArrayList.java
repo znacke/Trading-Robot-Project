6a8e7157df393253f342312e2588c933a6e2320e
HP <HP@10.192.102.150>
2019-02-01 18:52:39 +0100

    kj

---
+++ src/StockArrayList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class StockArrayList {
+
+	public static void main(String[] args) {
+		
+		/* Author: Bojan
+		 * Array Liste für Aktien erstellen
+		 */
+		
+		
+		//ArrayListe
+		ArrayList stock = new ArrayList<>();
+		ArrayList <String> namensListe = new ArrayList<String>();
+		
+		
+		//Daten
+		stock.add("Wasser");
+		stock.add("Brot");
+		//stock.add(new Integer(4));
+		
+		namensListe.add("Hans");
+		namensListe.add("Peter"); 
+		
+		//Datenausgabe
+		for(int i = 0; i<namensListe.size(); i++) {
+			namensListe.get(i);
+			System.out.println("Name: " + namensListe.get(i));
+		}
+			
+
+	}
+
+}
